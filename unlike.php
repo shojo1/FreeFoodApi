9be6454de9ef007b595fb647654606c6ceeb8f8d
shojo1 <shojo1@umbc.edu>
2017-10-16 18:52:48 -0400

    you can also unlike events. there's an api for that

---
+++ unlike.php
@@ -0,0 +1,26 @@
+<?php
+//This is the api for liking an event
+//GET Request must contain:
+//  hash
+//Returns success or failure
+
+  $db_host  = 'host';
+  $username = 'user';
+  $pwd  = 'pwd';
+  $database  = 'db';
+
+  if(isset($_GET['hash'])){
+    $hash = $_GET['hash'];
+
+    $pdo = new PDO('mysql:host='.$db_host.';dbname='.$database, $username, $pwd);
+        if(!$pdo)
+        {
+            $echo("Database Login failed! Please make sure that the DB login credentials provided are correct");
+            return false;
+        }
+    $query1 = $pdo->prepare("UPDATE events SET Likes = Likes - 1 WHERE Hash = '".$hash."';");
+    //Execute query
+    $query1->execute(array());
+
+  }
+?>
